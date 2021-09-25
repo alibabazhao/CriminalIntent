bd50ae1ad502f7b8395f7afb749f06451f40e955
Ali Zhao <0604aliz@gmail.com>
2021-09-24 22:48:58 -0400

    Toggle the new CheckBox and disabled Button that displays the date.

--- app/src/main/java/com/example/criminalintent/Crime.java
+++ app/src/main/java/com/example/criminalintent/Crime.java
@@ -1,13 +1,34 @@
 package com.example.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class Crime {
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
+    private boolean mSolved;
 
     public Crime(){
         mId=UUID.randomUUID();
+        mDate=new Date();
     }
 
     public UUID getId() {
