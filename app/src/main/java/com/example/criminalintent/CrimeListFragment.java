57214d64a15f05b1396ce244f157c3d7b787552c
Ali Zhao <0604aliz@gmail.com>
2021-10-02 16:12:30 -0400

    make each item clickable

--- app/src/main/java/com/example/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/example/criminalintent/CrimeListFragment.java
@@ -3,13 +3,18 @@ package com.example.criminalintent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ListFragment;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class CrimeListFragment extends ListFragment {
 
     private ArrayList<Crime> mCrimes;
+
+    private static final String TAG="CrimeListFragment";
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -19,5 +24,11 @@ public class CrimeListFragment extends ListFragment {
 
         ArrayAdapter<Crime> adapter=new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, mCrimes);
         setListAdapter(adapter);
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        Crime c=(Crime)(getListAdapter()).getItem(position);
+        Log.d(TAG, c.getTitle()+" was clicked");
     }
 }
