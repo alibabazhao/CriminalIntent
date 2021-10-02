66cf3fb19e8dfd7985964717dee5a67c0357a4f0
Ali Zhao <0604aliz@gmail.com>
2021-10-02 13:38:18 -0400

    create a listFragment and add an abstract activity to host fragment

--- app/src/main/java/com/example/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/example/criminalintent/CrimeActivity.java
@@ -1,22 +1,25 @@
 package com.example.criminalintent;
 
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
 
-public class CrimeActivity extends AppCompatActivity {
+public class CrimeActivity extends SingleFragmentActivity {
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_fragment);
+//
+//        FragmentManager fm=getSupportFragmentManager();
+//        Fragment fragment=fm.findFragmentById(R.id.fragmentContainer);
+//        if(fragment==null){
+//            fragment=new CrimeFragment();
+//            fm.beginTransaction().add(R.id.fragmentContainer, fragment).commit();
+//        }
+//    }
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_crime);
-
-        FragmentManager fm=getSupportFragmentManager();
-        Fragment fragment=fm.findFragmentById(R.id.fragmentContainer);
-        if(fragment==null){
-            fragment=new CrimeFragment();
-            fm.beginTransaction().add(R.id.fragmentContainer, fragment).commit();
-        }
+    protected Fragment createFragment() {
+        return new CrimeFragment();
     }
 }
